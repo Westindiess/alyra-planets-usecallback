47e381648a1de4d84d076645353a36381582baef
Westindiess <gregoryalexiss@gmail.com>
2021-05-31 20:46:42 +0200

    callback planets

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -2,19 +2,19 @@ import { useEffect } from "react"
 import Planet from "./Planet"
 
 const Planets = ({ planets, initFetch, fetchData }) => {
-  useEffect(() => {
-    initFetch()
-    fetchData()
-  }, [])
-  return (
-    <>
-      <div className="row">
-        {planets.map((planet) => {
-          return <Planet key={planet.name} planet={planet} />
-        })}
-      </div>
-    </>
-  )
+	useEffect(() => {
+		initFetch()
+		fetchData()
+	}, [initFetch, fetchData])
+	return (
+		<>
+			<div className="row">
+				{planets.map((planet) => {
+					return <Planet key={planet.name} planet={planet} />
+				})}
+			</div>
+		</>
+	)
 }
 
 export default Planets
